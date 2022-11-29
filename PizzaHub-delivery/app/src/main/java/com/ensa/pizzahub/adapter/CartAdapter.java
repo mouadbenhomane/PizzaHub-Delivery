9db7fa2242978fb21da830c41fda1d11435cf375
benhomanemouad <70215542+mouadbenhomane@users.noreply.github.com>
2022-11-29 18:43:32 +0100

    .

--- PizzaHub-delivery/app/src/main/java/com/ensa/pizzahub/adapter/CartAdapter.java
+++ PizzaHub-delivery/app/src/main/java/com/ensa/pizzahub/adapter/CartAdapter.java
@@ -6,58 +6,59 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.ensa.pizzahub.PizzaDetails;
 import com.ensa.pizzahub.R;
-import com.ensa.pizzahub.model.Popular;
+import com.ensa.pizzahub.model.OrderItem;
+import com.ensa.pizzahub.model.Pizza;
 
 import java.util.List;
 
-public class CartAdapter extends RecyclerView.Adapter<CartAdapter.PopularViewHolder> {
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.OrderViewHolder> {
 
     private Context context;
-    private List<Popular> popularList;
+    private List<OrderItem> orderItemListList;
+    public int i =0;
 
-    public CartAdapter(Context context, List<Popular> popularList) {
+    public CartAdapter(Context context, List<OrderItem> orderItemListList) {
         this.context = context;
-        this.popularList = popularList;
+        this.orderItemListList = orderItemListList;
     }
 
     @NonNull
     @Override
-    public PopularViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
+    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.cart_recycler_items, parent, false);
-         // here we need to create a layout for recyclerview cell items.
-
-
-        return new PopularViewHolder(view);
+        return new OrderViewHolder(view);
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
-    public void onBindViewHolder(@NonNull PopularViewHolder holder, @SuppressLint("RecyclerView") final int position) {
-        /*
-        holder.popularName.setText(popularList.get(position).getName());
+    public void onBindViewHolder(@NonNull final OrderViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.name.setText(orderItemListList.get(position).getPizza().getName());
+        holder.quantity.setText("x "+orderItemListList.get(position).getQuantity());
+        holder.size.setText("missed "+orderItemListList.get(position).getPizza().getId());
+        i++;
+        holder.price.setText(String.format("%.2f",orderItemListList.get(position).getPrice()));
+        holder.time.setText(String.format("%.2f",orderItemListList.get(position).getPizza().getDeliveryTime()));
+        holder.desc.setText(orderItemListList.get(position).getPizza().getDescription());
+        Glide.with(context).load(orderItemListList.get(position).getPizza().getImagePath()).into(holder.pizzaImage);
 
-        // for image we add Glide library dependency for image fetching from server
-
-        Glide.with(context).load(popularList.get(position).getImageUrl()).into(holder.popularImage);
-        */
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        holder.remove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(context, PizzaDetails.class);
-                i.putExtra("name", popularList.get(position).getName());
-                i.putExtra("price", popularList.get(position).getPrice());
-                i.putExtra("rating", popularList.get(position).getRating());
-                i.putExtra("image", popularList.get(position).getImageUrl());
-
-                context.startActivity(i);
+                //delete item from list and from DB
+                orderItemListList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, orderItemListList.size());
+                holder.itemView.setVisibility(View.GONE);
             }
         });
 
@@ -65,19 +66,26 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.PopularViewHol
 
     @Override
     public int getItemCount() {
-        return popularList.size();
+        return orderItemListList.size();
     }
 
-    public  static class PopularViewHolder extends RecyclerView.ViewHolder{
+    public  static class OrderViewHolder extends RecyclerView.ViewHolder{
 
-        ImageView popularImage;
-        TextView popularName;
+        ImageView pizzaImage;
+        TextView name,quantity,size,price,desc,time;
+        Button remove;
 
-        public PopularViewHolder(@NonNull View itemView) {
+        public OrderViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            popularName = itemView.findViewById(R.id.pizzaName);
-            popularImage = itemView.findViewById(R.id.all_menu_image);
+            name = itemView.findViewById(R.id.pizzaName);
+            quantity = itemView.findViewById(R.id.quantity);
+            size = itemView.findViewById(R.id.size);
+            price = itemView.findViewById(R.id.pirce);
+            desc = itemView.findViewById(R.id.desc);
+            time = itemView.findViewById(R.id.timeD);
+            pizzaImage = itemView.findViewById(R.id.pizzaPhoto);
+            remove = itemView.findViewById(R.id.remove);
 
         }
     }
